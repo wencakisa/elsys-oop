882826569a9ef4413c66a4aee5d00305a0cf70cf
wencakisa <wencakisa@gmail.com>
2017-09-23 15:07:49 +0300

    Added IO chapter

---
+++ cpp-101/chapter_08_iostreams/cin.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int input;
+
+    cout << "Enter a number: ";
+    cin >> input;
+
+    cout << "Input is " << input << endl;
+
+    return 0;
+}
---
+++ cpp-101/chapter_08_iostreams/cout.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "The answer is " << 42 << "!" << endl;
+    cout << "pi = " << 3.14 << endl;
+
+    return 0;
+}
